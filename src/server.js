c18b9c595bc038a3de621c8156ed6aec6e187db1
essias <contato@essias.com.br>
2026-03-18 17:41:27 -0300

    Update log message language to Spanish

--- src/server.js
+++ src/server.js
@@ -18,5 +18,5 @@ server.use(/^(?!\/(public|libros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rutas en la puerta 3000
 
 server.listen(8000, () => {
-	console.log("API disponível em http://localhost:8000")
+	console.log("API disponível en http://localhost:8000")
 })
