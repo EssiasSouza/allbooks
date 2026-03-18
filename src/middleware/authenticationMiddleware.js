de65d20b86deab9467f0376955b093eb91ebad49
essias <contato@essias.com.br>
2026-03-18 17:34:49 -0300

    Este es el primero commit de la nuestra clase donde no hemos cambiado nada solamente empezamos nuestro repositorio

---
+++ src/middleware/authenticationMiddleware.js
@@ -0,0 +1,29 @@
+// middlewares/authenticationMiddleware.js
+const { verifyToken } = require('../services/authService');
+
+function authenticationMiddleware(req, res, next) {
+    if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
+        const status = 401;
+        const message = 'Token no valido';
+        res.status(status).json({ status, message });
+        return;
+    }
+
+    try {
+        const verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+
+        if (verifyTokenResult instanceof Error) {
+            const status = 401;
+            const message = 'Token de autenticacion no existe';
+            res.status(status).json({ status, message });
+            return;
+        }
+        next();
+    } catch (err) {
+        const status = 401;
+        const message = 'Token revogado';
+        res.status(status).json({ status, message });
+    }
+}
+
+module.exports = authenticationMiddleware;
